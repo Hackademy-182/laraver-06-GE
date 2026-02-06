7a18c831403ce78de8719e16b8a5f334bd3d944c
gege-xia <GEGEXIA94@GMAIL.COM>
2026-02-06 15:41:34 +0100

    init project + bootstrap vite + x-layout + landing

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="it">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ $title ?? 'Marketplace' }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-light">
+    <div class="container py-4">{{ $slot }}</div>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout title="Auto & Moto">
+    <h1>Marketplace Auto & Moto</h1>
+    <p>Annunci di auto e moto nuove e usate.</p>
+    <a class="btn btn-primary me-2" href="/cars">Esplora Auto</a>
+    <a class="btn btn-outline-primary" href="/motorbikes">Esplora Moto</a>
+</x-layout>
